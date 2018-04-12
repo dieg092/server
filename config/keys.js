df2f789600c7f47c7fa425e4ad8b6915e24a86c2
Diego Barranco <diegobarranco92@gmail.com>
2018-04-12 17:53:17 +0200

    Fix error

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // key.js - figure out what of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // We are in poruction - return the prod set of keys
-  module.exports = require('./dev');
+  module.exports = require('./prod');
 } else {
   // We are in development - return the prod dev of keys
   module.exports = require('./dev');
